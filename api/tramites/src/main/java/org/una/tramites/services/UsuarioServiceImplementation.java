f9d7c08996f15cc8f1fd548db6cf97c1d9b76323
Gus2730 <gus2730@gmail.com>
2020-09-03 10:22:38 -0600

    cambios

--- api/tramites/src/main/java/org/una/tramites/services/UsuarioServiceImplementation.java
+++ api/tramites/src/main/java/org/una/tramites/services/UsuarioServiceImplementation.java
@@ -5,9 +5,16 @@
  */
 package org.una.tramites.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.una.tramites.entities.Usuario;
@@ -104,7 +111,7 @@ public class UsuarioServiceImplementation implements IUsuarioService {
     @Override
     @Transactional(readOnly = true)
     public Optional<List<Usuario>> findByEstadoContaining(boolean estado) {
-         return Optional.ofNullable(usuarioRepository.findByEstadoContaining(estado));
+        return Optional.ofNullable(usuarioRepository.findByEstadoContaining(estado));
     }
 
     @Override
